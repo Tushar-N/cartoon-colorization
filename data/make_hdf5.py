5a6dd3ac70ed3ab3e3efbba0f53386c4c89d028e
Tushar Nagarajan <tushar.nagarajan@gmail.com>
2016-11-15 23:14:32 -0600

    hdf5 script updated

--- data/make_hdf5.py
+++ data/make_hdf5.py
@@ -9,6 +9,7 @@ from sklearn.utils import shuffle as sk_shuffle
 from joblib import Parallel, delayed
 import os.path
 from sklearn.cross_validation import train_test_split
+import os
 
 #------------------------------util------------------------------#
 def read_lines(flname):
@@ -17,6 +18,19 @@ def read_lines(flname):
 
 def parallelize(func, itemlist, params={}, verbose=1):
 	return Parallel(n_jobs=20, verbose=verbose)(delayed(func)(item, params) for item in itemlist)
+
+def center_crop(img, size): # requires (b,ch,w,h)
+	new_width, new_height= size
+	_, _, width, height = img.shape
+
+	horiz = (width - new_width)
+	vert= (height - new_height)
+
+	left, top= horiz/2, vert/2	
+	right, bot = horiz-left, vert-top
+	
+	return img[:, :, left:-right, top:-bot]	
+	
 
 def read_img(fl, params): #skimage gives you an RGB, cv2 gives BGR
 	img=skimage.io.imread(fl)
@@ -43,7 +57,7 @@ def read_img(fl, params): #skimage gives you an RGB, cv2 gives BGR
 
 #----------------------------------------------------------------#
 
-def make_hdf5(filenames, out_file, shuffle=True):
+def make_hdf5(filenames, out_file, crop=None):
 
 
 	color_files=['raw/frames/%s'%f for f in filenames]
@@ -57,9 +71,13 @@ def make_hdf5(filenames, out_file, shuffle=True):
 	img_data=sk_shuffle(img_data)
 	color_imgs, sketch_imgs=zip(*img_data)
 	color_imgs= np.asanyarray(color_imgs)
-	grey_imgs=np.mean(color_imgs, 1, keepdims=True)
-	sketch_imgs= np.asanyarray(grey_imgs)
-	
+	sketch_imgs= np.asanyarray(sketch_imgs)	
+
+	# crop images if required
+	if crop is not None:
+		color_imgs=center_crop(color_imgs, crop)
+		sketch_imgs=center_crop(sketch_imgs, crop)
+
 	img_mean=np.mean(color_imgs, 0)
 
 	with h5py.File(out_file, 'w') as hf:
@@ -73,8 +91,14 @@ def make_hdf5(filenames, out_file, shuffle=True):
 img_files=[fl.split('/')[-1] for fl in glob.glob('raw/frames/*.png')]
 
 train_files, val_files, _, _ = train_test_split(img_files, [0]*len(img_files), test_size=0.2, random_state=42)
-make_hdf5(train_files, 'hdf5/train_data.h5', True)
-make_hdf5(val_files, 'hdf5/val_data.h5', False)
+make_hdf5(train_files, 'hdf5/train_data.h5', (176,176))
+make_hdf5(val_files, 'hdf5/val_data.h5') # already 256x256
+
+with open('hdf5/train.txt','w') as f:
+	f.write('%s/train_data.h5'%os.getcwd())
+with open('hdf5/test.txt','w') as f:
+	f.write('%s/test_data.h5'%os.getcwd())
+
 
 
 
