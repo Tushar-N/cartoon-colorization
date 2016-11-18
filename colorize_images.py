abb6b3d749a792bdc25522e7c357c982858339e7
Tushar <tushar.nagarajan@gmail.com>
2016-11-17 22:48:52 -0600

    deploy v5

--- colorize_images.py
+++ colorize_images.py
@@ -1,18 +1,20 @@
 import numpy as np
+import skimage.transform
+import skimage.color as color
+import scipy.ndimage.interpolation as sni
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import skimage.transform
+plt.rcParams['figure.figsize'] = (12, 6)
 
 import sys
 import os
 sys.path.append('%s/caffe-colorization/python'%os.getcwd())
 sys.path.append('%s/resources/'%os.getcwd())
-
 import caffe
-import os
-import skimage.color as color
-import scipy.ndimage.interpolation as sni
+caffe.set_mode_gpu()
+caffe.set_device(0)
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -23,41 +25,6 @@ parser.add_argument('--reference', default='data/raw/frames/11_246.png')
 parser.add_argument('--save', default='output/colorized.png')
 opt = parser.parse_args()
 
-# returns the L channel of the sketch
-def get_sketch(H_in, W_in):
-	# load the original image
-	img_rgb = caffe.io.load_image(opt.sketch)
-	img_rs = caffe.io.resize_image(img_rgb,(H_in,W_in)) # resize image to network input size
-
-	img_lab = color.rgb2lab(img_rgb) # convert image to lab color space
-	img_l = img_lab[:,:,0] # pull out L channel
-	(H_orig,W_orig) = img_rgb.shape[:2] # original image size
-
-	# resize image to network input size
-	img_rs = caffe.io.resize_image(img_rgb,(H_in,W_in))
-	img_lab_rs = color.rgb2lab(img_rs)
-	img_l_rs = img_lab_rs[:,:,0]
-	img_l_rs = img_l_rs-50 #subtract mean
-
-	return img_l_rs, H_orig, W_orig, img_l
-
-# returns the 3channel reference image
-def get_reference(H_in, W_in):
-
-	# load the original image
-	img_rgb = caffe.io.load_image(opt.reference)
-	img_rs = caffe.io.resize_image(img_rgb,(H_in,W_in)) # resize image to network input size
-	img_rs = np.transpose(img_rs, (2,0,1)) # right dims
-	img_rs = img_rs[::-1,:,:] #RGB to BGR
-
-	return img_rs
-
-
-#%matplotlib inline
-plt.rcParams['figure.figsize'] = (12, 6)
-
-caffe.set_mode_gpu()
-caffe.set_device(0)
 
 
 net = caffe.Net(opt.prototxt, opt.caffemodel, caffe.TEST)
@@ -68,22 +35,35 @@ net = caffe.Net(opt.prototxt, opt.caffemodel, caffe.TEST)
 pts_in_hull = np.load('resources/pts_in_hull.npy') # load cluster centers
 net.params['class8_ab'][0].data[:,:,0,0] = pts_in_hull.transpose((1,0)) # populate cluster centers as 1x1 convolution kernel
 
-img_l_rs, H_orig, W_orig, img_l= get_sketch(H_in, W_in)
-net.blobs['data_l'].data[0,0,:,:] = img_l_rs
+# Get sketch information
+sketch_rgb = caffe.io.load_image(opt.sketch)
+sketch_lab = color.rgb2lab(sketch_rgb)
+sketch_l = sketch_lab[:,:,0,np.newaxis] # pull out L channel
+(H_orig,W_orig) = sketch_rgb.shape[:2] # original image size
+
+sketch_rs = caffe.io.resize_image(sketch_rgb,(H_in,W_in))
+sketch_lab_rs = color.rgb2lab(sketch_rs)
+sketch_l_rs = img_lab_rs[:,:,0]
+sketch_l_rs = sketch_l_rs - 50
+net.blobs['data_l'].data[0,0,:,:] = sketch_l_rs
+
 try: # only push the reference if it exists
-	img_ref=get_reference(H_in, W_in)
-	net.blobs['col_reference_data'].data[0,:,:,:] = img_ref
+	ref_rgb = caffe.io.load_image(opt.reference)
+	ref_rs = caffe.io.resize_image(ref_rgb,(H_in,W_in))
+	ref_lab = color.rgb2lab(ref_rs)	
+	net.blobs['ref_lab'].data[0,:,:,:] = ref_lab
 except:
 	print 'cannot find layer'
 
 net.forward() # run network
 
-ab_dec = net.blobs['class8_ab'].data[0,:,:,:].transpose((1,2,0)) # this is our result
-ab_dec_us = sni.zoom(ab_dec,(1.*H_orig/H_out,1.*W_orig/W_out,1)) # upsample to match size of original image L
+# retrieve output and upsample
+ab_dec = net.blobs['class8_ab'].data[0,:,:,:].transpose((1,2,0))
+ab_dec_us = sni.zoom(ab_dec,(1.*H_orig/H_out,1.*W_orig/W_out,1))
 
-l_ch=img_l[:,:,np.newaxis]
-img_lab_out = np.concatenate((l_ch,ab_dec_us),axis=2) # concatenate with original image L
-img_rgb_out = np.clip(color.lab2rgb(img_lab_out),0,1) # convert back to rgb
+# concatenate with original image L, and convert to RGB
+img_lab_out = np.concatenate((sketch_l,ab_dec_us),axis=2) 
+img_rgb_out = np.clip(color.lab2rgb(img_lab_out),0,1) 
 
 plt.imshow(img_rgb_out);
 plt.axis('off');
