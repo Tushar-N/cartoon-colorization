48bd04eb9914ad82302ee338364006cb69534f5b
Tushar <tushar.nagarajan@gmail.com>
2016-11-17 19:37:53 -0600

    lmdb creation

--- data/create_lmdb.sh
+++ data/create_lmdb.sh
@@ -8,7 +8,7 @@ TOOLS=/work/04340/tushar_n/packages/caffe/build/tools
 
 FRAME_DATA_ROOT=/work/04340/tushar_n/cartoon-colorization/data/raw/frames/
 SKETCH_DATA_ROOT=/work/04340/tushar_n/cartoon-colorization/data/raw/sketch/
-REF_DATA_ROOT=/work/04340/tushar_n/cartoon-colorization/data/raw/frames/
+REF_DATA_ROOT=/work/04340/tushar_n/cartoon-colorization/data/raw/reference/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
@@ -41,7 +41,7 @@ fi
 #GLOG_logtostderr=1 $TOOLS/convert_imageset --gray\
 #    --resize_height=$RESIZE_HEIGHT \
 #    --resize_width=$RESIZE_WIDTH \
-#    $FRAME_DATA_ROOT \
+#    $SKETCH_DATA_ROOT \
 #    $DATA/ltrain.txt \
 #    $EXAMPLE/train_sketch_lmdb
 
@@ -49,7 +49,7 @@ echo "Creating val sketch lmdb..."
 GLOG_logtostderr=1 $TOOLS/convert_imageset --gray\
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
-    $FRAME_DATA_ROOT \
+    $SKETCH_DATA_ROOT \
     $DATA/lval.txt \
     $EXAMPLE/val_sketch_lmdb
 
